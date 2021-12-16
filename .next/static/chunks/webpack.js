4c07bb4c26a9362d66f472b44b067dcd39dbc2a2
Luis Lucas <luis@handcom.com.br>
2021-12-15 23:15:40 -0300

    Add article.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "36412538bca51189"; }
+/******/ 		__webpack_require__.h = function() { return "4c2e961dc0f910a2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
