67a2716f6a15f101031b7e91940c5f9eb02bb52e
Luis Lucas <luis@handcom.com.br>
2021-12-15 21:45:24 -0300

    Create logo.js

--- components/logo.js
+++ components/logo.js
@@ -21,13 +21,13 @@ const Logo = () => {
         <Link href="/">
             <a>
                 <LogoBox>
-                    <Image src={footPrintImg} width={20} height={20} alt="" />
+                    <Image src={footPrintImg} width={20} height={20} alt="logo" />
                     <Text 
                         color={useColorModeValue('gray.800', 'whiteAlpha.900')}
-                        fontFamily="M Plus Round 1c"
+                        fontFamily="M Plus Round 1c, sans-serif"
                         fontWeight="bold"
                         ml={3}>
-                          Luis Lucas  
+                          Luis Silva 
                     </Text>
                 </LogoBox>
             </a>
