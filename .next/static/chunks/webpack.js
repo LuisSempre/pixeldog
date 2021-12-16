e00c1c4c7cdfe39fa8ca3b9d930b240f375e0d92
Luis Lucas <luis@handcom.com.br>
2021-12-15 23:13:25 -0300

    Update index.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7d348ab1a400cc94"; }
+/******/ 		__webpack_require__.h = function() { return "36412538bca51189"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
